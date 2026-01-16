6fca17efe9714cb79fafd727f4c1f49a6532611e
Gabriel Stefanoni <gabrielpstefanoni@gmail.com>
2026-01-16 17:02:10 -0300

    feat: add jpa repositories

---
+++ backend/src/main/java/com/gabriel/authapi/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.gabriel.authapi.repository;
+
+import com.gabriel.authapi.domain.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+}
+
